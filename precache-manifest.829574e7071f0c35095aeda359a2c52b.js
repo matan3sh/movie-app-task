8aefe69993cd31d4370b626b815aef80982f337b
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-07-04 19:53:33 +0300

    Updates

---
+++ precache-manifest.829574e7071f0c35095aeda359a2c52b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42939b132352999bfa7e51e942b0f6ec",
+    "url": "/movie-app-task/index.html"
+  },
+  {
+    "revision": "df4bd351b15607df457e",
+    "url": "/movie-app-task/static/css/main.ebe83662.chunk.css"
+  },
+  {
+    "revision": "45e6954300e77bef768d",
+    "url": "/movie-app-task/static/js/2.fce8481b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app-task/static/js/2.fce8481b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4bd351b15607df457e",
+    "url": "/movie-app-task/static/js/main.9d021d3b.chunk.js"
+  },
+  {
+    "revision": "f25183ecc693c2c444ad",
+    "url": "/movie-app-task/static/js/runtime-main.8c51a6be.js"
+  }
+]);
